d982c0efe694e42a2d0a07456eac1eece91629a6
Mario Garcia <mario.rggj@gmail.com>
2023-01-27 19:20:30 -0300

    feature(data): create new migration to map Product.Price to decimal to the database

--- WebApplication1/Context/ApiContext.cs
+++ WebApplication1/Context/ApiContext.cs
@@ -10,4 +10,13 @@ public class ApiContext : DbContext
     }
 
     public DbSet<Product> Products { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Product>()
+            .Property(p => p.Price)
+            .HasColumnType("decimal(18,2)");
+        
+        base.OnModelCreating(modelBuilder);
+    }
 }
