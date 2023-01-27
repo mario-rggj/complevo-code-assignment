2508a5174cb35013bd675cfb929eb930f7739835
Mario Garcia <mario.rggj@gmail.com>
2023-01-27 17:53:45 -0300

    feat(data): create DbContext

---
+++ WebApplication1/Context/ApiContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WebApplication1.Context;
+
+public class ApiContext : DbContext
+{
+    public ApiContext(DbContextOptions<ApiContext> options) : base(options)
+    {
+    }
+}
--- WebApplication1/Program.cs
+++ WebApplication1/Program.cs
@@ -1,7 +1,11 @@
+using Microsoft.EntityFrameworkCore;
+using WebApplication1.Context;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Services.AddDbContext<ApiContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("Default")));
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
