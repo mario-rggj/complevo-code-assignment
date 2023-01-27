4e3e258f0d0af95e326783b09968a2cf95b2b7be
Mario Garcia <mario.rggj@gmail.com>
2023-01-27 17:57:24 -0300

    feat(data): add initial migration

--- WebApplication1/Program.cs
+++ WebApplication1/Program.cs
@@ -18,6 +18,17 @@ if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+    
+    using (var scope = app.Services.CreateScope())
+    {
+        var services = scope.ServiceProvider;
+
+        var context = services.GetRequiredService<ApiContext>();
+        if (context.Database.GetPendingMigrations().Any())
+        {
+            context.Database.Migrate();
+        }
+    }
 }
 
 app.UseHttpsRedirection();
